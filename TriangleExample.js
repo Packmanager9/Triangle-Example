59ce070794936a156e6bd36e61150d27d1da1993
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2020-11-06 21:07:13 -0800

    this one, not the other

---
+++ TriangleExample.js
@@ -0,0 +1,271 @@
+
+    class Triangle {
+        constructor(x, y, color, length, fill = 0, strokeWidth = 0, leg1Ratio = 1, leg2Ratio = 1, heightRatio = 1) {
+            this.x = x
+            this.y = y
+            this.color = color
+            this.length = length
+            this.x1 = this.x + this.length * leg1Ratio
+            this.x2 = this.x - this.length * leg2Ratio
+            this.tip = this.y + this.length * heightRatio
+            this.accept1 = (this.y - this.tip) / (this.x1 - this.x)
+            this.accept2 = (this.y - this.tip) / (this.x2 - this.x)
+            this.fill = fill
+            this.stroke = strokeWidth
+        }
+        draw() {
+            canvas_context.strokeStyle = this.color
+            canvas_context.stokeWidth = this.stroke
+            canvas_context.beginPath()
+            canvas_context.moveTo(this.x, this.y)
+            canvas_context.lineTo(this.x1, this.y)
+            canvas_context.lineTo(this.x, this.tip)
+            canvas_context.lineTo(this.x2, this.y)
+            canvas_context.lineTo(this.x, this.y)
+            if (this.fill == 1) {
+                canvas_context.fill()
+            }
+            canvas_context.stroke()
+            canvas_context.closePath()
+        }
+        isPointInside(point) {
+            if (point.x <= this.x1) {
+                if (point.y <= this.tip) {
+                    if (point.y >= this.y) {
+                        if (point.x >= this.x2) {
+                            this.accept1 = (this.y - this.tip) / (this.x1 - this.x)
+                            this.accept2 = (this.y - this.tip) / (this.x2 - this.x)
+                            this.basey = point.y - this.tip
+                            this.basex = point.x - this.x
+                            if (this.basex == 0) {
+                                return true
+                            }
+                            this.slope = (this.basey / this.basex)*-1
+                            if (this.slope <= this.accept1) {
+                                return true
+                            } else if (this.slope >= this.accept2) {
+                                return true
+                            }
+                        }
+                    }
+                }
+            }
+            return false
+        }
+    }
+
+
+    class Observer {
+        constructor(x, y, radius, color, range = 100, rays = 10, angle = (Math.PI * .125)) {
+            this.body = new Circle(x, y, radius, color)
+            this.color = color
+            this.ray = []
+            this.rayrange = range
+            this.globalangle = Math.PI
+            this.gapangle = angle
+            this.currentangle = 0
+            this.obstacles = []
+            this.raymake = rays
+        }
+        beam() {
+            this.currentangle = this.gapangle / 2
+            for (let k = 0; k < this.raymake; k++) {
+                this.currentangle += (this.gapangle / Math.ceil(this.raymake / 2))
+                let ray = new Circle(this.body.x, this.body.y, 1, "white", (((Math.cos(this.globalangle + this.currentangle)))), (((Math.sin(this.globalangle + this.currentangle)))))
+                ray.collided = 0
+                ray.lifespan = this.rayrange - 1
+                this.ray.push(ray)
+            }
+            for (let f = 0; f < this.rayrange; f++) {
+                for (let t = 0; t < this.ray.length; t++) {
+                    if (this.ray[t].collided < 1) {
+                        this.ray[t].move()
+                        for (let q = 0; q < this.obstacles.length; q++) {
+                            if (this.obstacles[q].isPointInside(this.ray[t])) {
+                                this.ray[t].collided = 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        draw() {
+            this.beam()
+            this.body.draw()
+            canvas_context.lineWidth = 1
+            canvas_context.fillStyle = this.color
+            canvas_context.strokeStyle = this.color
+            canvas_context.beginPath()
+            canvas_context.moveTo(this.body.x, this.body.y)
+            for (let y = 0; y < this.ray.length; y++) {
+                canvas_context.lineTo(this.ray[y].x, this.ray[y].y)
+                canvas_context.lineTo(this.body.x, this.body.y)
+            }
+            canvas_context.stroke()
+            canvas_context.fill()
+            this.ray = []
+        }
+    }
+
+    class Circle {
+        constructor(x, y, radius, color, xmom = 0, ymom = 0, friction = 1, reflect = 0, strokeWidth = 0, strokeColor = "transparent") {
+            this.x = x
+            this.y = y
+            this.radius = radius
+            this.color = color
+            this.xmom = xmom
+            this.ymom = ymom
+            this.friction = friction
+            this.reflect = reflect
+            this.strokeWidth = strokeWidth
+            this.strokeColor = strokeColor
+        }
+        draw() {
+            canvas_context.lineWidth = this.strokeWidth
+            canvas_context.strokeStyle = this.color
+            canvas_context.beginPath();
+            if (this.radius > 0) {
+                canvas_context.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), true)
+                canvas_context.fillStyle = this.color
+                canvas_context.fill()
+                canvas_context.stroke();
+            } else {
+                console.log("The circle is below a radius of 0, and has not been drawn. The circle is:", this)
+            }
+        }
+        move() {
+            if (this.reflect == 1) {
+                if (this.x + this.radius > canvas.width) {
+                    if (this.xmom > 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y + this.radius > canvas.height) {
+                    if (this.ymom > 0) {
+                        this.ymom *= -1
+                    }
+                }
+                if (this.x - this.radius < 0) {
+                    if (this.xmom < 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y - this.radius < 0) {
+                    if (this.ymom < 0) {
+                        this.ymom *= -1
+                    }
+                }
+            }
+            this.x += this.xmom
+            this.y += this.ymom
+        }
+        unmove() {
+            if (this.reflect == 1) {
+                if (this.x + this.radius > canvas.width) {
+                    if (this.xmom > 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y + this.radius > canvas.height) {
+                    if (this.ymom > 0) {
+                        this.ymom *= -1
+                    }
+                }
+                if (this.x - this.radius < 0) {
+                    if (this.xmom < 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y - this.radius < 0) {
+                    if (this.ymom < 0) {
+                        this.ymom *= -1
+                    }
+                }
+            }
+            this.x -= this.xmom
+            this.y -= this.ymom
+        }
+        frictiveMove() {
+            if (this.reflect == 1) {
+                if (this.x + this.radius > canvas.width) {
+                    if (this.xmom > 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y + this.radius > canvas.height) {
+                    if (this.ymom > 0) {
+                        this.ymom *= -1
+                    }
+                }
+                if (this.x - this.radius < 0) {
+                    if (this.xmom < 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y - this.radius < 0) {
+                    if (this.ymom < 0) {
+                        this.ymom *= -1
+                    }
+                }
+            }
+            this.x += this.xmom
+            this.y += this.ymom
+            this.xmom *= this.friction
+            this.ymom *= this.friction
+        }
+        frictiveunMove() {
+            if (this.reflect == 1) {
+                if (this.x + this.radius > canvas.width) {
+                    if (this.xmom > 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y + this.radius > canvas.height) {
+                    if (this.ymom > 0) {
+                        this.ymom *= -1
+                    }
+                }
+                if (this.x - this.radius < 0) {
+                    if (this.xmom < 0) {
+                        this.xmom *= -1
+                    }
+                }
+                if (this.y - this.radius < 0) {
+                    if (this.ymom < 0) {
+                        this.ymom *= -1
+                    }
+                }
+            }
+            this.xmom /= this.friction
+            this.ymom /= this.friction
+            this.x -= this.xmom
+            this.y -= this.ymom
+        }
+        isPointInside(point) {
+            this.areaY = point.y - this.y
+            this.areaX = point.x - this.x
+            if (((this.areaX * this.areaX) + (this.areaY * this.areaY)) <= (this.radius * this.radius)) {
+                return true
+            }
+            return false
+        }
+        doesPerimeterTouch(point) {
+            this.areaY = point.y - this.y
+            this.areaX = point.x - this.x
+            if (((this.areaX * this.areaX) + (this.areaY * this.areaY)) <= ((this.radius + point.radius) * (this.radius + point.radius))) {
+                return true
+            }
+            return false
+        }
+    } 
+
+
+
+    let tringle = new Triangle(350, 350, "yellow", 50)
+    let observer = new Observer(350, 500, 1, "red", 400, 30, Math.PI*.5)
+    observer.obstacles.push(tringle)
+
+    window.setInterval(function () {
+        tringle.draw()
+        observer.draw()
+    }, 17)
